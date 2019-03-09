eab669b0a7665cc84d5de184ee01ad624b75c389
erin-koen <erin.koen@gmail.com>
2019-03-09 15:38:25 -0500

    react methods for adding form data complete

--- src/EmailCollector.js
+++ src/EmailCollector.js
@@ -1,14 +1,40 @@
 import React from "react";
 import emailImg from "../src/Email_icon.png";
-import insta from "../src/insta.png";
+// import { encode } from "iconv-lite";
 // import { Input, Button } from 'react-strap';
+
+
+
+const encode=(data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
 
 class EmailCollector extends React.Component {
   constructor() {
     super();
     this.state = {
-      submitted: false
+      submitted: false,
+      email:"",
     };
+  }
+
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlecoded" },
+      body: encode({ "form-name": "email-collector", ...this.state})
+    })
+    .then(()=> alert("Success!"))
+    .catch(error=> alert(error))
+  };
+
+  handleChanges = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 
   clickHandler = e => {
@@ -24,15 +50,15 @@ class EmailCollector extends React.Component {
         <div className="submission">
           <form
             name="email-collector"
-            method="post" action="#" netlify
+            method="post" action="#" 
             onSubmit={this.clickHandler}
             className="email-collector"
 
-            // data-netlify="true"
+            data-netlify="true"
           >
             <div className="input">
               <img className="email-icon" alt="email-icon" src={emailImg} />
-              <input type="email" placeholder="what's your e-mail?" />
+              <input type="email" name="email" placeholder="what's your e-mail?" />
             </div>
             <br />
             <button type="submit">Join</button>
