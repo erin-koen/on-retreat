015e5b23bc5f8da7d1f7553af2c6e26d23e0502f
erin-koen <erin.koen@gmail.com>
2019-03-09 13:07:52 -0500

    form styled close-ish

--- src/EmailCollector.js
+++ src/EmailCollector.js
@@ -1,4 +1,5 @@
 import React from "react";
+import emailImg from "../src/Email_icon.png";
 // import { Input, Button } from 'react-strap';
 
 class EmailCollector extends React.Component {
@@ -9,32 +10,39 @@ class EmailCollector extends React.Component {
     };
   }
 
-  clickHandler = (e) => {
+  clickHandler = e => {
     e.preventDefault();
     this.setState({
-        submitted: true,
-    })
-  }
+      submitted: true
+    });
+  };
 
   render() {
     if (!this.state.submitted) {
       return (
-        <form onSubmit={this.clickHandler} className="email-collector" netlify data-netlify="true">
-        <input
-          type="text"
-          placeholder="  Our first collection goes live soon! Drop us your email to stay in touch."
-        />
+        <form
+          onSubmit={this.clickHandler}
+          className="email-collector"
+          netlify
+          data-netlify="true"
+        >
+        <div className="input">
+          <img className="email-icon" alt="email-icon" src={emailImg} />
+            <input
+              type="email"
+              placeholder="what's your e-mail?"
+            />
+        </div>
         <br />
-        <button color="link" type="submit">submit</button> 
-        </form> 
-      )
-    } 
-    else {
-      return (<h2>Thanks! We'll let you know when we've got the goods.</h2>);
+          <button color="link" type="submit">
+            Join
+          </button>
+        </form>
+      );
+    } else {
+      return <h2>Thanks! We'll let you know when we've got the goods.</h2>;
     }
   }
 }
 
 export default EmailCollector;
-
-
