25db97bb9deb90c761866e6b4a184f627611172f
erin-koen <erin.koen@gmail.com>
2019-03-09 12:10:26 -0500

    background bled to edges and image changed, css refactored a bit, really needs a preprocessor tho

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import logo from "../src/OnRetreat_Logo.png";
+
 import "./App.css";
 import Header from "./header";
+import Body from "./Body";
 import Footer from "./Footer";
 import EmailCollector from "./EmailCollector";
 
@@ -14,21 +15,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-body">
           <Header />
-          <div className="copy-body">
-            <h1>
-              on retreat is a vibrant <br />
-              collection of unique belongings <br />
-              found in the far corners <br />
-              of the world, curated by <br />
-              an eclectic group of expert travelers
-            </h1>
-            <EmailCollector className="email-input" />
-          </div>
-          
+          <Body />
+          <EmailCollector className="email-input" />
           <Footer />
-        </header>
       </div>
     );
   }
---
+++ src/Body.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Body = () => {
+  return (
+    <div className="body-copy">
+      <h1>
+        on retreat is a vibrant collection of unique belongings found in the far
+        corners of the world, curated by an eclectic group of expert travelers
+      </h1>
+      <br />
+      <br />
+      <div className="cta">
+        <h3>
+          Our first collection just landed from Cartagena. Subscribe below and
+          we'll let you know when it goes live!
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Body;
